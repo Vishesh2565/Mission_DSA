ab4107e5d4f48704feac6ce526b8eef17bdb4519
Vishesh2565 <vishesh25vs@gmail.com>
2024-02-20 22:23:55 +0530

    Final code for Full_Pyramid

---
+++ Full_Pyramid.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Full_Pyramid {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,k=0;
+        System.out.println("Enter the no. of lines");
+        n= sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=n+i;j++)
+            {
+                if((j>=n-i)&&(j<=n+i))
+                System.out.print("*");
+                else
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
