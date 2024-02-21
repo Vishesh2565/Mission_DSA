cf597e0cc97d4966fc5336f4d406c40998b81fd4
Vishesh2565 <vishesh25vs@gmail.com>
2024-02-22 00:59:59 +0530

    Final code for Hollow_Full_Pyramid

---
+++ Hollow_Full_Pyramid.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Hollow_Full_Pyramid {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,k=0;
+        System.out.println("Enter the no. of lines");
+        n= sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=n+i;j++)
+
+            {
+                if(i==n-1)
+                {
+                    if((j>=n-i)&&(j<=n+i)&&k==0)
+                    {
+                        System.out.print("*");
+                        k=1;
+                    }
+                    else{
+                        System.out.print(" ");
+                        k=0;
+                    }
+                }
+                else if((j==n-i)||(j==n+i)&&k==0)
+                {
+                System.out.print("*");
+                k=1;
+                }
+                else{
+                System.out.print(" ");
+                k=0;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
