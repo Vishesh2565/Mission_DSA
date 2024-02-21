e47a37c99bcd4a0651279b6016b1e017cc45561c
Vishesh2565 <vishesh25vs@gmail.com>
2024-02-21 23:38:18 +0530

    Final code for Diamond_Pattern

---
+++ Diamond_Pattern.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Diamond_Pattern {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int k=0;
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<=5+i;j++)
+
+            {
+                if((j>=5-i)&&(j<=5+i)&&k==0)
+                {
+                System.out.print("*");
+                k=1;
+                }
+                else{
+                System.out.print(" ");
+                k=0;
+                }
+            }
+            System.out.println();
+        }
+        for(int i=0;i<4;i++)
+        {
+            System.out.print(" ");
+            for(int j=0;j<4;j++)
+            {
+                if(j>=i)
+                System.out.print(" *");
+                else
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
