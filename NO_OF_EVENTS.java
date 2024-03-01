1e5b997c9ebc6a6ab9c973214c69cf9684f2677e
Vishesh2565 <vishesh25vs@gmail.com>
2024-03-01 20:59:34 +0530

    "Pine Tree" Company has signed up for a major event management deal from the Rotary Youth Club. A trade show is being held at the Codissia Complex where all startup companies in the software industry are showcasing their newest products and services and networking with industry partners and clients. To develop a piece of code that will collect user input regarding the number of events to be hosted for the fair in Codissia and show it.

---
+++ NO_OF_EVENTS.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class NO_OF_EVENTS {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        System.out.println("Enter the number of events");
+        n=sc.nextInt();
+        System.out.println("Number of events hosted in Codissia is "+n);
+    }
+}
