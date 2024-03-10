150b8cbfb41a2e850bed96ff9538d23d7966cd8d
Vishesh2565 <vishesh25vs@gmail.com>
2024-03-10 12:23:41 +0530

    Code to rotate the array

---
+++ rotation.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class rotation {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Enter the number for rotation");
+        int r=sc.nextInt();
+        for(int i=n-r;i<n;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        for(int i=0;i<n-r;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
